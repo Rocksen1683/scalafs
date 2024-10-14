73ea0d0518bbf1cb24ac5a513b5824c66493196f
Rocksen1683 <edpravee@uwaterloo.ca>
2024-10-14 15:29:35 -0400

    handling datanode failure and block re-allocation

--- src/main/scala/common/Messages.scala
+++ src/main/scala/common/Messages.scala
@@ -4,5 +4,5 @@ package common
 case class StoreBlock(blockId: String, data: Array[Byte])
 case class DataNodeHeartbeat(dataNodeId: String)
 case class CreateFile(fileName: String, fileSize: Long)
-case class BlockLocation(blockId: String, dataNodeIds: List[String])
+case class BlockLocation(blockId: String, var dataNodeIds: List[String])
 case class ReadBlock(blockId: String)
--- src/main/scala/namenode/NameNode.scala
+++ src/main/scala/namenode/NameNode.scala
@@ -1,91 +1,140 @@
 package namenode
 
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Cancellable, Props}
 import common._ 
 import scala.collection.mutable
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.StdIn
 
-// Case classes for message passing between actors
+//message passing between actors
 case class DeleteFile(fileName: String)
 case class GetFileBlocks(fileName: String)
-case class BlockLocation(blockId: String, dataNodeIds: List[String])
+case object CheckDataNodeFailures
+case class DataNodeFailure(dataNodeId: String)
 
 class NameNode extends Actor {
   
-  // Metadata: File name -> List of block locations
-  private val fileMetadata: mutable.Map[String, List[BlockLocation]] = mutable.Map()
+    //Metadata: File name -> List of block locations
+    private val fileMetadata: mutable.Map[String, List[BlockLocation]] = mutable.Map()
 
-  // Registered DataNodes
-  private val dataNodes: mutable.Map[String, ActorRef] = mutable.Map()
+    //set of datanodes registered withi this namenode
+    private val dataNodes: mutable.Map[String, (ActorRef, Long)] = mutable.Map()
 
-  // Replication factor
-  val replicationFactor = 3
+    private var checkFailureTask: Option[Cancellable] = None
 
-  // Incoming messages
-  def receive: Receive = {
+    override def preStart(): Unit = {
+    checkFailureTask = Some(context.system.scheduler.scheduleWithFixedDelay(0.seconds, 5.seconds, self, CheckDataNodeFailures))
+    }
+
+    override def postStop(): Unit = {
+    checkFailureTask.foreach(_.cancel())
+    }
+
+    //replication factor -> number of blocks that will replicate data
+    val replicationFactor = 3
+
+    // Incoming messages
+    def receive: Receive = {
     case CreateFile(fileName, fileSize) =>
-      println(s"Creating file: $fileName with size $fileSize")
-      val blocks = allocateBlocks(fileName, fileSize)
-      fileMetadata.put(fileName, blocks)
-      
-      // Instruct DataNodes to store blocks
-      blocks.foreach { block =>
-        val blockData = Array.fill(128 * 1024 * 1024)(0.toByte) // Simulated block data
-        block.dataNodeIds.foreach { dataNodeId =>
-          val dataNodeRef = dataNodes(dataNodeId)
-          dataNodeRef ! StoreBlock(block.blockId, blockData)
+        println(s"Creating file: $fileName with size $fileSize")
+        val blocks = allocateBlocks(fileName, fileSize)
+        fileMetadata.put(fileName, blocks)
+        
+        blocks.foreach { block =>
+            val blockData = Array.fill(128 * 1024 * 1024)(0.toByte) 
+            block.dataNodeIds.foreach { dataNodeId =>
+                val (dataNodeRef, _) = dataNodes(dataNodeId) 
+                dataNodeRef ! StoreBlock(block.blockId, blockData)
+            }
         }
-      }
 
-      sender() ! blocks // Send block locations back to client
-    
+
+        sender() ! blocks //send block locations back to client
+
     case DeleteFile(fileName) =>
-      println(s"Deleting file: $fileName")
-      fileMetadata.remove(fileName)
-    
-    case GetFileBlocks(fileName) =>
-      println(s"Fetching blocks for file: $fileName")
-      sender() ! fileMetadata.get(fileName).getOrElse(List())
-    
-    case DataNodeHeartbeat(dataNodeId) =>
-      println(s"Received heartbeat from DataNode: $dataNodeId")
-      if (!dataNodes.contains(dataNodeId)) {
-        dataNodes += (dataNodeId -> sender())
-      }
-  }
+        println(s"Deleting file: $fileName")
+        fileMetadata.remove(fileName)
 
-  //Method to allocate blocks to DataNodes
-  private def allocateBlocks(fileName: String, fileSize: Long): List[BlockLocation] = {
+    case GetFileBlocks(fileName) =>
+        println(s"Fetching blocks for file: $fileName")
+        sender() ! fileMetadata.get(fileName).getOrElse(List())
+
+    case DataNodeHeartbeat(dataNodeId) =>
+        println(s"Received heartbeat from DataNode: $dataNodeId")
+        dataNodes(dataNodeId) = (sender(), System.currentTimeMillis())
+
+    case CheckDataNodeFailures =>
+        checkDataNodeFailures()
+
+    case DataNodeFailure(dataNodeId) =>
+        handleDataNodeFailure(dataNodeId)
+    }
+
+    //allocate blocks to DataNodes
+    private def allocateBlocks(fileName: String, fileSize: Long): List[BlockLocation] = {
     val blockCount = Math.ceil(fileSize.toDouble / 128).toInt
     (1 to blockCount).map { i =>
-      val blockId = s"$fileName-block-$i"
-      val dataNodeIds = pickDataNodes(replicationFactor)
-      BlockLocation(blockId, dataNodeIds)
+        val blockId = s"$fileName-block-$i"
+        val dataNodeIds = pickDataNodes(replicationFactor)
+        BlockLocation(blockId, dataNodeIds)
     }.toList
-  }
-
-  //Pick multiple DataNodes for block replication
-  private def pickDataNodes(n: Int): List[String] = {
-    val dataNodeList = dataNodes.keys.toList
-    if (dataNodeList.size >= n) {
-      (0 until n).map { i =>
-        dataNodeList((i + currentDataNodeIndex) % dataNodeList.size)
-      }.toList
-    } else {
-      throw new Exception("Not enough DataNodes available for replication")
     }
-  }
 
-  private var currentDataNodeIndex = 0
+    //pick multiple DataNodes for block replication
+    private def pickDataNodes(n: Int): List[String] = {
+        val dataNodeList = dataNodes.keys.toList
+        if (dataNodeList.size >= n) {
+        (0 until n).map { i =>
+            dataNodeList((i + currentDataNodeIndex) % dataNodeList.size)
+        }.toList
+        } else {
+        throw new Exception("Not enough DataNodes available for replication")
+        }
+    }
+
+    //method to check datanode failures  
+    private def checkDataNodeFailures(): Unit = {
+        val currentTime = System.currentTimeMillis()
+        dataNodes.foreach { case (dataNodeId, (actorRef, lastHeartbeat)) =>
+            if (currentTime - lastHeartbeat > 10000) {
+            println(s"DataNode $dataNodeId is considered failed.")
+            self ! DataNodeFailure(dataNodeId)
+            }
+        }
+        }
+
+
+    //method to handle a particular datanode failure
+    private def handleDataNodeFailure(failedDataNodeId: String): Unit = {
+    println(s"Handling failure of DataNode $failedDataNodeId")
+
+    //remove the failed DataNode from the set 
+    dataNodes.remove(failedDataNodeId)
+
+    //replication policy
+    fileMetadata.foreach { case (fileName, blockList) =>
+        blockList.foreach { block =>
+        if (block.dataNodeIds.contains(failedDataNodeId)) {
+            println(s"Re-replicating block ${block.blockId} due to DataNode failure.")
+            val newDataNodeId = pickDataNodes(1).head //pick new datanode for replication
+            block.dataNodeIds = block.dataNodeIds.filterNot(_ == failedDataNodeId) :+ newDataNodeId
+            val blockData = Array.fill(128 * 1024 * 1024)(0.toByte) 
+            context.actorSelection(s"akka://DistributedFileSystem/user/$newDataNodeId") ! StoreBlock(block.blockId, blockData)
+        }
+        }
+    }
+    }
+
+    private var currentDataNodeIndex = 0
 }
 
 object NameNodeApp extends App {
-  val system: ActorSystem = ActorSystem("DistributedFileSystem")
-  val nameNode: ActorRef = system.actorOf(Props[NameNode], "NameNode")
+    val system: ActorSystem = ActorSystem("DistributedFileSystem")
+    val nameNode: ActorRef = system.actorOf(Props(new NameNode), "NameNode")
 
-  // Simulate creating a file after some delay
-  system.scheduler.scheduleOnce(5.seconds) {
-    nameNode ! CreateFile("example.txt", 512) // Create a 512MB file
-  }(system.dispatcher)
+
+    system.scheduler.scheduleOnce(5.seconds) {
+    nameNode ! CreateFile("example.txt", 512) 
+    }(system.dispatcher)
 }
